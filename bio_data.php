017892d0f6a9d6c880ff6e39729833327d207be4
JASMINE <130382185+jazmieyyy@users.noreply.github.com>
2024-02-03 21:37:38 +0530

    Update bio_data.php

--- bio_data.php
+++ bio_data.php
@@ -2,28 +2,39 @@
 an HTML page. -->
 
 <!DOCTYPE html>
-<html>
+<head>
+    <title>BioData</title>
+</head>
 <body>
-    <form action="" method="POST">
-        <H1>Biodata</h1>
-        Name:<br><input type=text name="name1"><br><br>
-        Address:<br> <textarea name=address> </textarea><br><br>
-        Age:<br> <input type=text name=age><br><br>
-        Phone:<br> <input type=number name=phone><br><br>
-        Email:<br> <input type=email name=email><br><br>
-        Educational Qualification:<br><input type=text name=qualification><br><br>
-        <input type=submit value=Display><br><br>
+    <h2>Enter the details</h2>
+    <form method="post">
+        Name: <br><input type="text" name="name" required><br>
+        Age: <br><input type="text" name="age" required><br>
+        Phone Number: <br><input type="tel" name="phone" required><br>
+        Email: <br><input type="email" name="email" required><br>
+        Address: <br><textarea name="address" required></textarea><br>
+        Gender: <br><input type="radio" id="male" name="gender" value="male" required>male 
+        <input type="radio" id="female" name="gender" value="female" required>female
+        <input type="radio" id="other" name="gender" value="other" required>Other <br>
+        <input type="submit" name="submit">
     </form>
+    <?php
+    if(isset($_POST['submit'])) {
+        $name = isset($_POST['name']) ? $_POST['name'] : '';
+        $age = isset($_POST['age']) ? $_POST['age'] : '';
+        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $address = isset($_POST['address']) ? $_POST['address'] : '';
+        $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
+    
+    echo "<h2>BIO DATA</h2>";
+    echo "<p><strong>Name:</strong> $name</p>"; 
+    echo "<p><strong>Age:</strong> $age</p>"; 
+    echo "<p><strong>Phone Number:</strong> $phone</p>";
+    echo "<p><strong>Email:</strong> $email</p>"; 
+    echo "<p><strong>Address:</strong> $address</p>";
+    echo "<p><strong>Gender:</strong> $gender</p>";
+    }
+    ?>
 </body>
 </html>
-<?php
-if ($_POST) {
-    echo "<h1>Biodata</h1>";
-    echo "Name: " . $_POST["name1"] . "<br>";
-    echo "Address: " . $_POST["address"] . "<br>";
-    echo "Age: " . $_POST["age"] . "<br>";
-    echo "Mobile: " . $_POST["phone"] . "<br>";
-    echo "Email: " . $_POST["email"] . "<br>";
-    echo "Qualification: " . $_POST["qualification"] . "<br>";
-}
-?>
